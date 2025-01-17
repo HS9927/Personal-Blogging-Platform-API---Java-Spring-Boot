0fa2f93de05e2be17e071d3922bc1cabe57c9c2d
HS9927 <hengsok2202@gmail.com>
2025-01-18 02:20:41 +0700

    prepare ui

---
+++ src/main/java/com/project/personal_blog/controller/PersonalBlogController.java
@@ -0,0 +1,16 @@
+package com.project.personal_blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blog")
+public class PersonalBlogController {
+
+	@GetMapping({"", "/"})
+	public String index () {
+		return "index";
+	}
+	
+}
